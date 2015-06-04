2da91f410d0eea91461808b611f7e22687677f2e
Suhyun Lee <raysuhyunlee@gmail.com>
2015-06-04 17:49:44 +0900

    parser added

--- proxy.c
+++ proxy.c
@@ -31,14 +31,65 @@ ssize_t Rio_readn_w(int fd, void *ptr, size_t nbytes);
 ssize_t Rio_readlineb_w(rio_t *rp, void *usrbuf, size_t maxlen);
 void Rio_writen_w(int fd, void *usrbuf, size_t n);
 
-void openclient(char* host, int port) {
+void parse_line(char* input) {
+	char* parsed[3] = {NULL, NULL, NULL};
+	int host_max = 30;
+	char host[host_max+1];
+	int port;
+	int cnt=0, i, len;
+	len = strlen(input);
+	parsed[cnt++] = input;
+	for(i = 0; i < len - 1 && cnt < 3; i++) {
+		if (input[i] == ' ') {
+			if (cnt == 1) {
+				strncpy(host, parsed[0], i<host_max? i : host_max);
+				host[i<host_max? i:host_max] = '\0';
+			}
+			parsed[cnt++] = input+i+1;
+		}
+	}
+	
+	if (host == NULL || parsed[1] == NULL || parsed[2] == NULL) {
+		printf("proxy usage: <host> <port> <message>\n");
+	} else if ((port = atoi(parsed[1])) == 0) {
+		printf("proxy usage: <host> <port> <message>\n");
+	} else {
+		printf("%s, %d, %s\n", host, port, parsed[2]); 
+	}
+}
+
+void openclient(char* host, int port, char* msg_send, char* msg_recv) {
 	int sockfd;
 	struct sockaddr_in server;
-	char *msg_send[100], *msg_recv[100];
+	int recvlen;
 
 	sockfd = socket(PF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1) {
 		printf("Failed to create client socket.\n");
+		exit(1);
+	}
+
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = inet_addr(host);
+
+	if (connect(sockfd, (struct sockaddr*)&server, sizeof(server)) == -1) {
+		printf("Failed to connect to server.\n");
+		exit(1);
+	}
+
+	if (write(sockfd, msg_send, strlen(msg_send)) == -1) {
+		printf("Failed to write.\n");
+		exit(1);
+	}
+
+	if ((recvlen = read(sockfd, msg_recv, strlen(msg_recv))) == -1) {
+		printf("Failed to read.\n");
+		exit(1);
+	}
+
+	if (close(sockfd) == -1) {
+		printf("Failed to close.\n");
 		exit(1);
 	}
 }
@@ -89,6 +140,7 @@ void openserver(int port) {
 			msg_recv[readlen] = NULL;
 			printf("len: %d, recv: %s", readlen, msg_recv);
 			fflush(stdout);
+
 			write(connfd, msg_recv, readlen);
 		}
 		
@@ -106,6 +158,7 @@ void openserver(int port) {
 int main(int argc, char **argv)
 {
 	FILE* fp;
+	char test[100];
 
 	/* open a file for logging */
 	fp = fopen("./proxy.log", "w+");
@@ -116,7 +169,10 @@ int main(int argc, char **argv)
 			exit(0);
 	}
 
-	openserver(atoi(argv[1]));
+	//openserver(atoi(argv[1]));
+	parse_line("blabla blabla blabla");
+	parse_line("blabla 3000 blabla");
+	parse_line("blabla 3000 ");
 		
 	/* close log file */
 	fclose(fp);
